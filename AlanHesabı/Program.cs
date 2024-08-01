bcad2f49da98e6d6f366fc9c3902c3d86884542a
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-08-01 17:27:06 +0300

    fibonacci series average implemented

---
+++ AlanHesabı/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace AlanHesabı
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ortalaması bulunmak istenen Fibonacci Basamak Sayısını Girin:");
+            int basamak = Convert.ToInt32(Console.ReadLine());
+
+            if (basamak <= 0)
+            {
+                Console.WriteLine("Lütfen pozitif bir sayı girin.");
+                return;
+            }
+
+            int fiboToplam = 0;
+            for (int i = 0; i < basamak; i++)
+            {
+                fiboToplam += Fibonacci(i);
+            }
+
+            Console.WriteLine("Fibonacci Toplamı: " + fiboToplam);
+            Console.WriteLine("Bulunan Ortalama: " + ortalamaAl(basamak, fiboToplam));
+            Console.ReadLine();
+        }
+
+        public static int Fibonacci(int basamak)
+        {
+            // 0 tabanlı rekürsif Fibonacci
+            if (basamak <= 1) return basamak;
+            return Fibonacci(basamak - 1) + Fibonacci(basamak - 2);
+        }
+
+        public static double ortalamaAl(int basamak, int fibo)
+        {
+            // Ortalama hesaplaması
+            return (double)fibo / basamak;
+        }
+    }
+}
