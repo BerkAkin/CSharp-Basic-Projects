53fafb6650aa993cbdf8c818cc07fbb1717d241e
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-08-01 17:29:45 +0300

    initial commit for Ucgen

---
+++ UcgenCizme/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UcgenCizme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
