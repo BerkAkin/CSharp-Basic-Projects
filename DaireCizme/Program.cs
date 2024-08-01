48375bf2135819a8b8f7f422d2e530708ef48579
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-08-01 19:15:20 +0300

    implemented daire uygulaması

---
+++ DaireCizme/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace DaireCizme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Dairenin çapını girin (genişlik ve yükseklik olarak):");
+            int diameter = int.Parse(Console.ReadLine());
+
+            DrawCircle(diameter);
+        }
+
+        static void DrawCircle(int diameter)
+        {
+            int radius = diameter / 2;
+            for (int y = 0; y < diameter; y++)
+            {
+                for (int x = 0; x < diameter; x++)
+                {
+                    // Dairenin içindeki noktaları hesapla
+                    double distance = Math.Sqrt(Math.Pow(x - radius, 2) + Math.Pow(y - radius, 2));
+
+                    if (distance > radius - 0.5 && distance < radius + 0.5)
+                    {
+                        Console.Write('*');
+                    }
+                    else
+                    {
+                        Console.Write(' ');
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+}
