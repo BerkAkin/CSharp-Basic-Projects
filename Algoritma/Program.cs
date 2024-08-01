72be1d302710336e9da8e68f90b00344a3c3c491
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-08-01 21:11:09 +0300

    homework algoritma string remove implemented

--- Algoritma/Program.cs
+++ Algoritma/Program.cs
@@ -6,7 +6,28 @@ namespace Algoritma
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(Kaldir("Algoritma", 22));
+        }
+
+        public static string Kaldir(string degisecek, int index)
+        {
+            if (degisecek.Length == 0)
+            {
+                Console.WriteLine("Değişecek Cümleyi Girmediniz");
+                return "";
+            }
+            else
+            {
+                if (index > degisecek.Length)
+                {
+                    Console.WriteLine("Index cümle uzunluğunun dışında kalıyor");
+                    return degisecek;
+                }
+                else
+                {
+                    return degisecek.Remove(index, 1).ToString();
+                }
+            }
         }
     }
 }
