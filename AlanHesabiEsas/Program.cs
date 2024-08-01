5ee4bc1e3cbe8fa783ac32be408e4b43b881e6c7
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-08-02 02:12:23 +0300

    initial commit for Alan Hesabı hw

---
+++ AlanHesabiEsas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlanHesabiEsas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
