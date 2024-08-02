878618be9e6626857f2bda765d5912daeecbea46
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-08-02 03:40:55 +0300

    created Sekil abstract class for the shape classes

---
+++ AlanHesabiEsas/Sekil.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace AlanHesabiEsas
+{
+    public abstract class Sekil : ISekil
+    {
+        public abstract double AlanHesapla();
+        public abstract double CevreHesapla();
+        public abstract double HacimHesapla();
+    }
+
+
+}
