dc7f2e989cb2ccbf2b290e833704b4d5a0022cfe
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-08-02 15:55:35 +0300

    İkiliToplam hw implemented

--- IkiliToplam/Program.cs
+++ IkiliToplam/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace IkiliToplam
 {
@@ -6,7 +7,44 @@ namespace IkiliToplam
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ArrayList verilen = new ArrayList() { 5, 63, 7, 4, 73, 3, 3, 3 };
+            ArrayList ar = IkiliToplam(verilen);
+
+            foreach (int i in ar)
+            {
+                Console.Write(i + " ");
+            }
+        }
+
+        public static ArrayList IkiliToplam(ArrayList arr)
+        {
+            int sayi1 = 0, sayi2 = 0;
+            ArrayList arrayList = new ArrayList();
+
+            for (int j = 0; j <= arr.Count - 1; j += 2)
+            {
+                if (j + 1 < arr.Count)
+                {
+                    sayi1 = Convert.ToInt32(arr[j]);
+                    sayi2 = Convert.ToInt32(arr[j + 1]);
+                    if (sayi1 == sayi2)
+                    {
+                        arrayList.Add((int)Math.Pow((sayi1 + sayi1), 2));
+                    }
+                    else
+                    {
+                        arrayList.Add(sayi1 + sayi2);
+                    }
+                }
+                else
+                {
+                    int lastElement = Convert.ToInt32(arr[j]);
+                    arrayList.Add(lastElement);
+                }
+            }
+
+
+            return arrayList;
         }
     }
 }
