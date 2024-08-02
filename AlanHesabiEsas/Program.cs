632de9483e8247b5e5a0687432ea944fce0f9e1a
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-08-02 03:41:49 +0300

    main program implemented

--- AlanHesabiEsas/Program.cs
+++ AlanHesabiEsas/Program.cs
@@ -6,7 +6,44 @@ namespace AlanHesabiEsas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Şekil Seçin (Daire,Üçgen,Kare,Dikdörtgen)");
+            string sekilSecim = Console.ReadLine().ToLower();
+            Sekil sekil = null;
+
+            switch (sekilSecim)
+            {
+                case "daire":
+                    Console.WriteLine("Yarıçap Girin:");
+                    double yaricap = Convert.ToDouble(Console.ReadLine());
+                    sekil = new Daire(yaricap);
+                    break;
+                case "üçgen":
+                    Console.WriteLine("Kenar Uzunluğu Girin:");
+                    double kenarUcgen = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Kenar Uzunluğu Girin:");
+                    double yukseklikUcgen = Convert.ToDouble(Console.ReadLine());
+                    sekil = new Ucgen(kenarUcgen, yukseklikUcgen);
+                    break;
+                case "dikdörtgen":
+                    Console.WriteLine("Uzun Kenar Girin");
+                    double uzunDik = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Kısa Kenar Girin");
+                    double kisaDik = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Derinlik Girin");
+                    double derinDik = Convert.ToDouble(Console.ReadLine());
+                    sekil = new Dikdortgen(uzunDik, kisaDik, derinDik);
+                    break;
+                case "kare":
+                    Console.WriteLine("Kenar Uzunkuğu Girin");
+                    double kenarKare = Convert.ToDouble(Console.ReadLine());
+                    sekil = new Kare(kenarKare);
+                    break;
+            }
+
+            Console.WriteLine("Hesaplama Seçin (Alan,Hacim,Çevre)");
+            string hesapSecim = Console.ReadLine().ToLower();
+            Hesaplayici.Hesapla(sekil, hesapSecim);
+
         }
     }
 }
