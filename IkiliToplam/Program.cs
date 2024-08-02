311c6ebab8ee7ed3d3c31e6345ab416bb9defcf6
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-08-02 15:25:53 +0300

    initial commit for IkiliToplam

---
+++ IkiliToplam/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IkiliToplam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
