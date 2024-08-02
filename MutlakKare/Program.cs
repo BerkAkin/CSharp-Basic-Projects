4b6b649424394c5a0bda54ffd5024e1d325de672
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-08-02 15:58:54 +0300

    initial commit for MutlakDeger hw

---
+++ MutlakKare/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MutlakKare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
