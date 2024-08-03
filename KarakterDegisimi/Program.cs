1a8a4cf321a0f06b1dc01e73d49259d5609ccb2e
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-08-03 18:23:56 +0300

    hw karakter değişimi implemented

--- KarakterDegisimi/Program.cs
+++ KarakterDegisimi/Program.cs
@@ -6,7 +6,18 @@ namespace KarakterDegisimi
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string cumle = "What's your blood made of";
+            KarakterDegis(cumle);
+        }
+        public static void KarakterDegis(string cumle)
+        {
+            if (string.IsNullOrEmpty(cumle) || cumle.Length == 1)
+            {
+                Console.WriteLine(cumle);
+            }
+            char[] chars = cumle.ToCharArray();
+            (chars[0], chars[chars.Length - 1]) = (chars[chars.Length - 1], chars[0]);
+            Console.WriteLine(new string(chars));
         }
     }
 }
