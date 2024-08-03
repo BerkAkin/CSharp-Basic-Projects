fdd5b92ebdb7d02ce38db0b8c7e3d58e884a9b7c
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-08-03 18:10:22 +0300

    homework mutlakkare implemented

--- MutlakKare/Program.cs
+++ MutlakKare/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.Collections;
 
 namespace MutlakKare
 {
     class Program
     {
+        const int Number = 67;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ArrayList arrayList = new ArrayList() { 13, 78, 45, 10, 45 };
+            ArrayList mutlakListe = MutlakKare(arrayList);
+            foreach (int i in mutlakListe)
+            {
+                Console.Write(i + " ");
+            }
+        }
+        public static ArrayList MutlakKare(ArrayList arr)
+        {
+            ArrayList donecekDizi = new ArrayList();
+            int Kucukler = 0;
+            int Buyukler = 0;
+
+            foreach (int num in arr)
+            {
+                if (num < 67)
+                {
+                    Kucukler += (67 - num);
+                }
+                else
+                {
+                    int buyuk = 67 - num;
+                    Buyukler += (int)Math.Pow(buyuk, 2);
+                }
+            }
+            donecekDizi.Add(Kucukler);
+            donecekDizi.Add(Buyukler);
+            return donecekDizi;
         }
     }
 }
