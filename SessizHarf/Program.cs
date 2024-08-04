1a1558e14cdb7a30c884f795ce66c6e9e259146d
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-08-05 01:26:15 +0300

    Sessiz Harf Bulma implemented

--- SessizHarf/Program.cs
+++ SessizHarf/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SessizHarf
 {
     class Program
     {
+        public static string sessizler = "bcçdfghjklmnpqrstvwyz";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine(SessizBul("Merhabalar"));
+        }
+
+        public static bool SessizBul(string cumle)
+        {
+            char[] arr = cumle.ToCharArray();
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (sessizler.Contains(arr[i]) && sessizler.Contains(arr[i + 1]))
+                {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
