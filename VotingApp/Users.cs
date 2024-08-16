a8c27c66725ab5d9e43ae6745d8b4b92a4a65952
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-08-16 23:22:32 +0300

    FEAT: added voteStatus to prevent same user vote again

--- VotingApp/Users.cs
+++ VotingApp/Users.cs
@@ -6,11 +6,11 @@ namespace VotingApp
     class Users
     {
         private static List<User> users = new List<User>{
-            new User("BERK", "1234"),
-            new User("AKIN", "1234"),
-            new User("DENİZ", "1234"),
+            new User("BERK", "1234", false),
+            new User("AKIN", "1234", false),
+            new User("DENİZ", "1234", false),
         };
-        public static List<User> UserList { get => users; }
+        public static List<User> UserList { get => users; set => users = value; }
         public static void AddUser(User user)
         {
             UserList.Add(user);
