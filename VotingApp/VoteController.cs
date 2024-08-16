6f9bfebbd7e202d826693426cc57211844c46d51
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-08-16 23:22:49 +0300

    FEAT: Added continous voting system

--- VotingApp/VoteController.cs
+++ VotingApp/VoteController.cs
@@ -9,16 +9,32 @@ namespace VotingApp
         {
             Console.Write("Oylama İşlemi Başladı. Kategoriler: ");
             Console.WriteLine("HORROR, COMEDY, ROMANCE, DRAMA, ACTION, WESTERN");
+            int voteStat = 1;
+            while (voteStat > 0)
+            {
+                Console.WriteLine("Oy Veren Kişinin Adı?");
+                string name = Console.ReadLine();
 
-            Console.WriteLine("Oy Veren Kişinin Adı?");
-            string name = Console.ReadLine();
+                UserController.Register(name);
 
-            UserController.Register(name);
+                Console.WriteLine("Oy Verilecek Kategori?");
+                string category = Console.ReadLine();
 
-            Console.WriteLine("Oy Verilecek Kategori?");
-            string category = Console.ReadLine();
+                ActionVote(category, name);
 
-            ActionVote(category, name);
+                Console.WriteLine("Oy verme işlemi devam edecek mi?");
+                int islem = Convert.ToInt32(Console.ReadLine());
+                if (islem > 0)
+                {
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+
 
         }
         public void ActionVote(string vote, string voter)
@@ -30,7 +46,7 @@ namespace VotingApp
             }
             else
             {
-                Categories.VoteToCategory(vote.ToUpper());
+                Categories.VoteToCategory(vote);
                 Console.WriteLine("Oylama Tamamlandı. Oranları görmek için 1'e basınız");
                 short secim = Convert.ToInt16(Console.ReadLine());
                 if (secim == 1)
