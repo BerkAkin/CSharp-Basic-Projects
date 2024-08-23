af0af9dd054cf92dad6be9fbf29ab66aa2e04c2e
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-08-23 23:05:28 +0300

    feat: added finduser method for userlogin logic

--- ATM/Users.cs
+++ ATM/Users.cs
@@ -1,13 +1,19 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ATM
 {
-    public class Users
+    public static class Users
     {
-        public List<User> Kullanicilar = new List<User>() {
-            new User("Berk","Akın",1234),
+        public static List<User> Kullanicilar = new List<User>() {
+            new User("Berk","Akın",1111,1234),
             new User("Cem","Karaca",1234),
         };
+
+        public static User FindUser(int accountNum, int Password)
+        {
+            return Kullanicilar.FirstOrDefault(user => user.AccountNumber == accountNum && user.Password == Password);
+        }
 
     }
 
