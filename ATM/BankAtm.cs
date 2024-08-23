98d6ba689471231b86488093073d733dcd4a8f3e
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-08-23 22:45:28 +0300

    included  login controller methods

--- ATM/BankAtm.cs
+++ ATM/BankAtm.cs
@@ -2,6 +2,32 @@ namespace ATM
 {
     class BankAtm
     {
+        private bool _loggedIn = false;
+        private int _loggedInUserAccNum = 0;
 
+        public void userLogin()
+        {
+            this._loggedIn = true;
+            this._loggedInUserAccNum = 1;//kullanıcının hesap numarası bulunmalı öncelikle;
+        }
+        public void userLogout()
+        {
+            this._loggedIn = false;
+            this._loggedInUserAccNum = 0;
+        }
+
+        public bool isLogged()
+        {
+            if (this._loggedIn && this._loggedInUserAccNum > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+                //giriş işlemini yeniden yap ve sisteme log düş kullanıcı hatalı giriş yaptı diye
+            }
+
+        }
     }
 }
