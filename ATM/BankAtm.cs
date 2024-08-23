335f5b306473337d6025728cb2e7f018cbe13fc7
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-08-23 23:04:15 +0300

    feat: added logic for userLogin and UserLogout

--- ATM/BankAtm.cs
+++ ATM/BankAtm.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
 namespace ATM
 {
     class BankAtm
@@ -5,15 +9,53 @@ namespace ATM
         private bool _loggedIn = false;
         private int _loggedInUserAccNum = 0;
 
+        //sistemin başlayacağı nokta burası login ekranı ile başlayacak
         public void userLogin()
         {
-            this._loggedIn = true;
-            this._loggedInUserAccNum = 1;//kullanıcının hesap numarası bulunmalı öncelikle;
+            if (isLogged())
+            {
+                Console.WriteLine("Giriş Yapmış Bir Kullanıcı Zaten Mevcut Çıkış Yapmak İçin 1'e Basın");
+                int option = Convert.ToInt32(Console.ReadKey());
+                if (option == 1)
+                {
+                    userLogout();
+                }
+                else
+                {
+                    Console.WriteLine("Hatalı Bir Seçim Yaptınız. Sistem Kapatılıyor.");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Giriş Yap");
+                Console.Write("Hesap Numarası: ");
+                int accountNum = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+                Console.Write("Şifre: ");
+                int password = Convert.ToInt32(Console.ReadLine());
+
+                //kullanıcının hesap numarası bulunacak ve sonra aşağıdaki değerler değişecek;
+                User user = Users.FindUser(accountNum, password);
+                if (user != null)
+                {
+                    this._loggedIn = true;
+                    this._loggedInUserAccNum = user.AccountNumber;
+                }
+                else
+                {
+                    Console.WriteLine("Kullanıcı Bulunamadı");
+                }
+
+            }
+
         }
+
         public void userLogout()
         {
             this._loggedIn = false;
             this._loggedInUserAccNum = 0;
+            Console.WriteLine("Başarıyla Çıkış Yapıldı.");
         }
 
         public bool isLogged()
