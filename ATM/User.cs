817e7dc83484f72fb38eb8f2cc1cdaa5b3894ca0
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-08-23 22:43:43 +0300

    created user entity for storing user information

---
+++ ATM/User.cs
@@ -0,0 +1,34 @@
+using System;
+namespace ATM
+{
+    public class User
+    {
+        private string _name;
+        private string _surname;
+        private int _accountNum;
+        private int _password;
+        private int _balance;
+        public string Name { get => this._name; set => this._name = value; }
+        public string Surname { get => this._surname; set => this._surname = value; }
+        public int AccountNumber { get => this._accountNum; }
+        public int Password { get => this._password; set => this._password = value; }
+        public int Balance { get => this._balance; set => this._balance = value; }
+
+        public User(string name, string surname, int Password)
+        {
+            this._name = name;
+            this._surname = surname;
+            this._accountNum = AccountNumCreator();
+            this._password = Password;
+            this._balance = 0;
+        }
+
+        private int AccountNumCreator()
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(100000, 999999);
+            return randomNumber;
+        }
+
+    }
+}
