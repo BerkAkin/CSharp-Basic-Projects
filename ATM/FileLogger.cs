d69a68e0a72bac5c60bbff42bcdebc481930bfb5
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-08-26 21:43:51 +0300

    feat: implemented file logging system

--- ATM/FileLogger.cs
+++ ATM/FileLogger.cs
@@ -1,7 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
 namespace ATM
 {
-    class FileLogger
+    public static class FileLogger
     {
+        private static string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        private static List<Log> transActions = new List<Log>();
+        private static List<Log> frauds = new List<Log>();
+
+
+        public static void EndOfDay()
+        {
+
+            string fileName = $"EOD_{DateMaker()}.txt";
+            string filePath = Path.Combine(desktopPath, fileName);
+
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                writer.WriteLine("Bilgilendirme Mesajları:");
+                foreach (Log log in transActions)
+                {
+                    writer.WriteLine(log.Date + " " + log.Message);
+                }
+
+                writer.WriteLine();
+
+                writer.WriteLine("Hatalı Denemeler:");
+                foreach (Log fraud in frauds)
+                {
+                    writer.WriteLine(fraud.Date + " " + fraud.Message);
+                }
+            }
+
+            Console.WriteLine($"EOD file written to {filePath}");
+        }
+
+        private static string DateMaker()
+        {
+            return DateTime.Now.ToString("ddMMyyyy");
+        }
+
+        public static void SetTransactionLogs(string islem)
+        {
+            Log log = new Log(DateMaker(), islem);
+            transActions.Add(log);
+        }
+        public static void SetFraudLogs(string islem)
+        {
+            Log log = new Log(DateMaker(), islem);
+            frauds.Add(log);
+        }
 
     }
 }
