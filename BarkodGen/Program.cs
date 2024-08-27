be3b13860b4377b81090a60d7fc8070ba86b4e31
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-08-27 16:30:10 +0300

    initial commit

---
+++ BarkodGen/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BarkodGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
