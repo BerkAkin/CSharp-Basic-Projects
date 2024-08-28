3124ee644d512c8120cce8e882541433c3c63b26
Berk Akın <berkakin99@hotmail.com>
2024-08-28 18:09:11 +0300

    included IronBarcode to project

--- BarkodGen/Program.cs
+++ BarkodGen/Program.cs
@@ -6,7 +6,11 @@ namespace BarkodGen
     {
         static void Main(string[] args)
         {
-
+            /*
+            Barcode Generator/Reader
+            3rd party barcode kütüphaneleri kullanarak barcode üreten bunu client'ın bilgisayarında bir lokasyona kaydeden. 
+            Daha sonra da barcode'u okuyabilen bir console uygulaması yazınız.
+            */
         }
     }
 }
